02c51601d9c632c3d08c567739f2dc1b7a496997
Jose Diaz <jose@exoreaction.com>
2024-10-10 21:20:09 -0500

    usando starter validation

--- src/main/java/pe/joedayz/thymeleaf/dto/CreateStudentDTO.java
+++ src/main/java/pe/joedayz/thymeleaf/dto/CreateStudentDTO.java
@@ -1,13 +1,42 @@
 package pe.joedayz.thymeleaf.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
 
 @Getter
 @Setter
 public class CreateStudentDTO {
-    private String id;
+
+    @NotEmpty(message = "{NotEmpty.name}")
     private String name;
+
+    @Email
+    @NotEmpty(message = "{NotEmpty.email}")
     private String email;
+
+    @NotNull(message = "{NotNull.birthday}")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+
+    @NotEmpty(message = "{NotEmpty.street}")
+    private String street;
+
+    @NotEmpty(message = "{NotEmpty.number}")
+    private String number;
+
+    @NotEmpty(message = "{NotEmpty.district}")
+    private String district;
+
+    @NotEmpty(message = "{NotEmpty.city}")
+    private String city;
+
+    @NotEmpty(message = "{NotEmpty.state}")
+    private String state;
 
 }
